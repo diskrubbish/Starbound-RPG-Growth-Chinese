52af2053e28f8387060ec2149be682d9964fafd7
diskrubbish <234511.cool@163.com>
2020-05-16 12:58:31 +0800

    5-16 回来了

--- script/tools/extract_all_labels.py
+++ script/tools/extract_all_labels.py
@@ -1,6 +1,6 @@
 from extract_labels import extract_labels
 from extract_patch_labels import extract_patch_labels
-root_dir = "F:\Starbound-RPG-Growth"
+root_dir = "F:/Starbound-RPG-Growth"
 prefix = "F:/Starbound-RPG-Growth-Chinese/translations"
 if __name__ == "__main__":
     print("1：Extract the normal labels")
